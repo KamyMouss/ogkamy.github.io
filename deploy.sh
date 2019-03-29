66b0ed6855282d3ec89c999f215dac5697b26648
Kamy Moussavi <32441857+ogKamy@users.noreply.github.com>
2019-03-29 11:00:45 -0400

    final deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-git checkout master && shopt -s extglob && rm !deploy.sh && git checkout source -- $(git ls-tree --name-only -r source | grep -E 'build/*') && git mv build/* . && rm -rf build && git add --all && git commit -m \"deployed\" && git checkout source
+git checkout master && shopt -s extglob && rm !deploy.sh && git checkout source -- $(git ls-tree --name-only -r source | grep -E 'build/*') && git mv build/* . && rm -rf build && git add --all && git commit -m \"deployed\" && git push && git checkout source
